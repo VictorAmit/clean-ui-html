84b697f0eabad425aa55f8946a6f94d146f9a052
Frontend <hello@mediatec.org>
2018-11-07 18:44:00 +0300

    w3c validation / fixes / updates

--- src/components/menu-left/common/menu-left.cleanui.js
+++ src/components/menu-left/common/menu-left.cleanui.js
@@ -1,92 +1,96 @@
-$(function () {
+/////////////////////////////////////////////////////////////////////////////////////////
+// "menu-right" module scripts
 
-  // scripts for "menu-left" module
+(function($) {
+  "use strict";
+  $(function () {
 
-  /////////////////////////////////////////////////////////////////////////////////////////
-  // add backdrop
+    /////////////////////////////////////////////////////////////////////////////////////////
+    // add backdrop
 
-  $('.menu-left').after('<div class="menu-left__backdrop menu-left__action--backdrop-toggle"><!-- --></div>')
+    $('.menu-left').after('<div class="menu-left__backdrop menu-left__action--backdrop-toggle"><!-- --></div>')
 
-  /////////////////////////////////////////////////////////////////////////////////////////
-  // submenu
+    /////////////////////////////////////////////////////////////////////////////////////////
+    // submenu
 
-  $('.menu-left__submenu > a').on('click', function () {
+    $('.menu-left__submenu > a').on('click', function () {
 
-    if ($('body').hasClass('config--vertical') || $('body').width() < 768) {
+      if ($('body').hasClass('config--vertical') || $('body').width() < 768) {
 
-      var parent = $(this).parent(),
-        opened = $('.menu-left__submenu--toggled')
+        var parent = $(this).parent(),
+          opened = $('.menu-left__submenu--toggled')
 
-      if (!parent.hasClass('menu-left__submenu--toggled') && !parent.parent().closest('.menu-left__submenu').length)
-        opened.removeClass('menu-left__submenu--toggled').find('> .menu-left__list').slideUp(200)
+        if (!parent.hasClass('menu-left__submenu--toggled') && !parent.parent().closest('.menu-left__submenu').length)
+          opened.removeClass('menu-left__submenu--toggled').find('> .menu-left__list').slideUp(200)
 
-      parent.toggleClass('menu-left__submenu--toggled')
-      parent.find('> .menu-left__list').slideToggle(200)
+        parent.toggleClass('menu-left__submenu--toggled')
+        parent.find('> .menu-left__list').slideToggle(200)
 
+      }
+
+    })
+
+    // remove submenu toggle class when viewport back to full view
+    $(window).on('resize', function () {
+      if ($('body').hasClass('config--horizontal') || $('body').width() > 768) {
+        $('.menu-left__submenu--toggled').removeClass('menu-left__submenu--toggled').find('> .menu-left__list').attr('style', '')
+      }
+    })
+
+    /////////////////////////////////////////////////////////////////////////////////////////
+    // custom scroll init
+
+    if ($('body').hasClass('config--vertical')) {
+      if (!(/Mobi/.test(navigator.userAgent)) && jQuery().perfectScrollbar) {
+        $('.menu-left__inner').perfectScrollbar({
+          theme: 'cleanui'
+        })
+      }
     }
 
-  })
+    /////////////////////////////////////////////////////////////////////////////////////////
+    // toggle menu
 
-  // remove submenu toggle class when viewport back to full view
-  $(window).on('resize', function () {
-    if ($('body').hasClass('config--horizontal') || $('body').width() > 768) {
-      $('.menu-left__submenu--toggled').removeClass('menu-left__submenu--toggled').find('> .menu-left__list').attr('style', '')
-    }
-  })
+    $('.menu-left__action--menu-toggle').on('click', function () {
+      if ($('body').width() < 768) {
+        $('body').toggleClass('menu-left--visible--mobile')
+      } else {
+        $('body').toggleClass('menu-left--visible')
+      }
+    })
 
-  /////////////////////////////////////////////////////////////////////////////////////////
-  // custom scroll init
+    $('.menu-left__action--backdrop-toggle').on('click', function () {
+      $('body').removeClass('menu-left--visible--mobile')
+    })
 
-  if ($('body').hasClass('config--vertical')) {
-    if (!(/Mobi/.test(navigator.userAgent)) && jQuery().perfectScrollbar) {
-      $('.menu-left__inner').perfectScrollbar({
-        theme: 'cleanui'
+    /////////////////////////////////////////////////////////////////////////////////////////
+    // colorful menu
+
+    var colorfulClasses = 'menu-left--colorful--primary menu-left--colorful--secondary menu-left--colorful--primary menu-left--colorful--default menu-left--colorful--info menu-left--colorful--success menu-left--colorful--warning menu-left--colorful--danger menu-left--colorful--yellow',
+      colorfulClassesArray = colorfulClasses.split(' ')
+
+    function setColorfulClasses() {
+      $('.menu-left__list--root > .menu-left__item').each(function () {
+        var randomClass = colorfulClassesArray[Math.floor(Math.random() * colorfulClassesArray.length)]
+        $(this).addClass(randomClass)
       })
     }
-  }
 
-  /////////////////////////////////////////////////////////////////////////////////////////
-  // toggle menu
-
-  $('.menu-left__action--menu-toggle').on('click', function () {
-    if ($('body').width() < 768) {
-      $('body').toggleClass('menu-left--visible--mobile')
-    } else {
-      $('body').toggleClass('menu-left--visible')
+    function removeColorfulClasses() {
+      $('.menu-left__list--root > .menu-left__item').removeClass(colorfulClasses)
     }
-  })
 
-  $('.menu-left__action--backdrop-toggle').on('click', function () {
-    $('body').removeClass('menu-left--visible--mobile')
-  })
+    if ($('body').hasClass('menu-left--colorful')) {
+      setColorfulClasses()
+    }
 
-  /////////////////////////////////////////////////////////////////////////////////////////
-  // colorful menu
-
-  var colorfulClasses = 'menu-left--colorful--primary menu-left--colorful--secondary menu-left--colorful--primary menu-left--colorful--default menu-left--colorful--info menu-left--colorful--success menu-left--colorful--warning menu-left--colorful--danger menu-left--colorful--yellow',
-    colorfulClassesArray = colorfulClasses.split(' ')
-
-  function setColorfulClasses () {
-    $('.menu-left__list--root > .menu-left__item').each(function () {
-      var randomClass = colorfulClassesArray[Math.floor(Math.random() * colorfulClassesArray.length)]
-      $(this).addClass(randomClass)
+    $('body').on('setColorfulClasses', function () {
+      setColorfulClasses()
     })
-  }
 
-  function removeColorfulClasses () {
-    $('.menu-left__list--root > .menu-left__item').removeClass(colorfulClasses)
-  }
+    $('body').on('removeColorfulClasses', function () {
+      removeColorfulClasses()
+    })
 
-  if ($('body').hasClass('menu-left--colorful')) {
-    setColorfulClasses()
-  }
-
-  $('body').on('setColorfulClasses', function () {
-    setColorfulClasses()
   })
-
-  $('body').on('removeColorfulClasses', function () {
-    removeColorfulClasses()
-  })
-
-})
+})(jQuery)
--- src/components/menu-right/common/menu-right.cleanui.js
+++ src/components/menu-right/common/menu-right.cleanui.js
@@ -1,84 +1,87 @@
 /////////////////////////////////////////////////////////////////////////////////////////
 // "menu-right" module scripts
-$(function () {
+(function($) {
+  "use strict";
+  $(function () {
 
-  /////////////////////////////////////////////////////////////////////////////////////////
-  // toggle right menu
+    /////////////////////////////////////////////////////////////////////////////////////////
+    // toggle right menu
 
-  $('.menu-right__action--menu-toggle').on('click', function () {
-    $('body').toggleClass('menu-right--visible')
-  })
-
-  /////////////////////////////////////////////////////////////////////////////////////////
-  // custom scroll init
-
-  if (!(/Mobi/.test(navigator.userAgent)) && jQuery().jScrollPane) {
-    $('.menu-right').each(function () {
-      $(this).jScrollPane({
-        contentWidth: '0px',
-        autoReinitialise: true,
-        autoReinitialiseDelay: 100
-      })
-      var api = $(this).data('jsp'),
-        throttleTimeout
-      $(window).bind('resize', function () {
-        if (!throttleTimeout) {
-          throttleTimeout = setTimeout(function () {
-            api.reinitialise()
-            throttleTimeout = null
-          }, 50)
-        }
-      })
+    $('.menu-right__action--menu-toggle').on('click', function () {
+      $('body').toggleClass('menu-right--visible')
     })
-  }
 
-  /////////////////////////////////////////////////////////////////////////////////////////
-  // options scripts
+    /////////////////////////////////////////////////////////////////////////////////////////
+    // custom scroll init
 
-  $('.menu-right--example-option').each(function () {
-
-    var inputs = $(this).find('input'),
-      buttons = $(this).find('.btn')
-
-    // detect current options and set active buttons
-    var found = false
-    inputs.each(function () {
-
-      if ($('body').hasClass($(this).val())) {
-        $(this).parent().trigger('click')
-        found = true
-      }
-
-    })
-    if (!found) {
-      $(this).find('input[value=""]').parent().trigger('click')
-      $('.menu-right .jspPane').css({top: 0})
+    if (!(/Mobi/.test(navigator.userAgent)) && jQuery().jScrollPane) {
+      $('.menu-right').each(function () {
+        $(this).jScrollPane({
+          contentWidth: '0px',
+          autoReinitialise: true,
+          autoReinitialiseDelay: 100
+        })
+        var api = $(this).data('jsp'),
+          throttleTimeout
+        $(window).on('resize', function () {
+          if (!throttleTimeout) {
+            throttleTimeout = setTimeout(function () {
+              api.reinitialise()
+              throttleTimeout = null
+            }, 50)
+          }
+        })
+      })
     }
 
-    // change options on click
-    $(this).find('.btn').on('click', function () {
-      var removeClasses = '',
-        addClass = $(this).find('input').val()
+    /////////////////////////////////////////////////////////////////////////////////////////
+    // options scripts
 
-      buttons.removeClass('active')
-      $(this).addClass('active')
+    $('.menu-right--example-option').each(function () {
 
+      var inputs = $(this).find('input'),
+        buttons = $(this).find('.btn')
+
+      // detect current options and set active buttons
+      var found = false
       inputs.each(function () {
-        removeClasses += $(this).val() + ' '
+
+        if ($('body').hasClass($(this).val())) {
+          $(this).parent().trigger('click')
+          found = true
+        }
+
+      })
+      if (!found) {
+        $(this).find('input[value=""]').parent().trigger('click')
+        $('.menu-right .jspPane').css({top: 0})
+      }
+
+      // change options on click
+      $(this).find('.btn').on('click', function () {
+        var removeClasses = '',
+          addClass = $(this).find('input').val()
+
+        buttons.removeClass('active')
+        $(this).addClass('active')
+
+        inputs.each(function () {
+          removeClasses += $(this).val() + ' '
+        })
+
+        $('body').removeClass(removeClasses).addClass(addClass)
+
+        if ($(this).find('input').attr('name') == 'options-colorful' && $(this).find('input').val() == 'menu-left--colorful') {
+          $('body').trigger('removeColorfulClasses')
+          $('body').trigger('setColorfulClasses')
+        }
+
+        if ($(this).find('input').attr('name') == 'options-colorful' && $(this).find('input').val() == '') {
+          $('body').trigger('removeColorfulClasses')
+        }
       })
 
-      $('body').removeClass(removeClasses).addClass(addClass)
-
-      if ($(this).find('input').attr('name') == 'options-colorful' && $(this).find('input').val() == 'menu-left--colorful') {
-        $('body').trigger('removeColorfulClasses')
-        $('body').trigger('setColorfulClasses')
-      }
-
-      if ($(this).find('input').attr('name') == 'options-colorful' && $(this).find('input').val() == '') {
-        $('body').trigger('removeColorfulClasses')
-      }
     })
 
   })
-
-})
+})(jQuery)
