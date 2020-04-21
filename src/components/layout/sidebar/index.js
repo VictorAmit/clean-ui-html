654897505b6cc095df49652c9f5eff909b1ddece
nick <nick@mediatec.org>
2020-04-21 22:33:02 +0300

    sidebar (settings) component

--- src/components/layout/sidebar/index.js
+++ src/components/layout/sidebar/index.js
@@ -31,6 +31,67 @@
     })
 
     /////////////////////////////////////////////////////////////////////////////////////////
+    // app name
+    function updateName(name) {
+      window.localStorage.setItem('appName', name)
+      var el = $('.cui__menuLeft').length ? $('.cui__menuLeft__logo__name') : $('.cui__menuTop__logo__name')
+      var descr = $('.cui__menuLeft').length ? $('.cui__menuLeft__logo__descr') : $('.cui__menuTop__logo__descr')
+      el.html(name)
+      if (name !== 'Clean UI Pro') {
+        descr.hide()
+      } else {
+        descr.show()
+      }
+    }
+    $('#appName').on('keyup', function (e) {
+      var value = e.target.value
+      updateName(value)
+    })
+    var appName = window.localStorage.getItem('appName')
+    if (appName) {
+      updateName(appName)
+      $('#appName').val(appName)
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////////
+    // set primary color
+    function setPrimaryColor(color) {
+      function setColor(_color) {
+        window.localStorage.setItem('kit.primary', _color)
+        var tag = '<style />'
+        var css = `:root { --kit-color-primary: ${_color};}`
+        $(tag)
+          .attr('id', 'primaryColor')
+          .text(css)
+          .prependTo('body')
+      }
+      var style = $('#primaryColor')
+      style ? (style.remove(), setColor(color)) : setColor(color)
+    }
+    var color = window.localStorage.getItem('kit.primary')
+    if (color) {
+      $('#colorPicker').val(color)
+      setPrimaryColor(color)
+      $('#resetColor')
+        .parent()
+        .removeClass('reset')
+    }
+    $('#colorPicker').on('change', function () {
+      var value = $(this).val()
+      setPrimaryColor(value)
+      $('#resetColor')
+        .parent()
+        .removeClass('reset')
+    })
+    $('#resetColor').on('click', function () {
+      window.localStorage.removeItem('kit.primary')
+      $('#primaryColor').remove()
+      $('#resetColor')
+        .parent()
+        .addClass('reset')
+    })
+
+    /////////////////////////////////////////////////////////////////////////////////////////
     // switch
     $('.cui__sidebar__switch input').on('change', function () {
       var el = $(this)
